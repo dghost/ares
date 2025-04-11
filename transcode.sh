e2a4b6a866c2291fddcc12f7a77e3aa5f7be3229
Luke Groeninger <dev@dghost.net>
2025-04-11 16:25:54 -0500

    Move audio files into their own directory

--- transcode.sh
+++ transcode.sh
@@ -4,7 +4,7 @@ cd radio/
 for file in *; do 
     if [ -d "$file" ]; then 
     	cd $file
-    	ffmpeg -i manifest.m3u ../$file.m4a
+    	ffmpeg -i manifest.m3u ../../radio_m4a/$file.m4a
     	cd ..
     fi 
 done
